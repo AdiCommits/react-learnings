e042841924ccb42d707cce8322cd51dad1af8efa
AdiCommits <maadityaa99@gmail.com>
2024-03-16 22:37:34 +0530

    Used nested components

--- booklist-app/src/index.js
+++ booklist-app/src/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function Greeting() {
-  return React.createElement("h2", {}, "Welcome to Booklist app");
+  return (
+    <div>
+      <Title />
+      <Caption />
+    </div>
+  );
 }
+
+const Title = () => <h2>Welcome to Booklist-app</h2>;
+const Caption = () => <p>This is where the caption goes</p>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
